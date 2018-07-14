3a8741f5e88f388c2a8ffe9d684db91b76343f69
Cristiana Costa <cristianacmc@gmail.com>
2018-07-14 23:16:27 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Movie Trailer Website
+
+This is a project that will show my favorite movies, along with its respective **box art imagery** and **movie trailer**.
+
+
+## Usage
+In order to run the this project you should have all files media.py, entertainment_center.py and fresh_tomatoes.py in the same folder.  
+
+ 
+## Definition
+
+_media.py_ is a file for the class definition Movie
+
+_entertainment_center.py_ is a file to define multiple instances of the class Movie
+
+_fresh_tomatoes.py_ has a function inside called open_movies_page that takes in a list of movies as input and as an output it creates and open html page that shows the movies you gave it in the first place. It stores the html folder in the same folder as the others movie program files are stored. 
+
+
+## Licence & copyright
+
+Cristiana Costa, Student at Udacity
+
---
+++ entertainment_center.py
@@ -0,0 +1,45 @@
+import media
+#media contains the class Movie that will be used to create the instances. 
+import fresh_tomatoes
+
+     
+#creating instances of the class Movie amd passing the values for instances_variables.
+forrest_gump = media.Movie("Forrest Gump",                        
+                         "A story of an extraordinary man that never forgot his childhood love",
+                         "http://static.tvtropes.org/pmwiki/pub/images/Forrest_Gump_on_a_bench_4829.jpg",
+                         "https://www.youtube.com/watch?v=bLvqoHBptjg")
+
+
+great_gatsby = media.Movie("The Great Gatsby",
+                          "A man that is drawn into the captivating world of the wealthy and witness illusions and deceits",
+                          "http://t2.gstatic.com/images?q=tbn:ANd9GcRHjdRa754KlBw3wFJaa8pxGHbESpYGpN2Pt0A63Wgseu2f42Jd",
+                          "https://www.youtube.com/watch?v=rARN6agiW7o")
+
+sword_fish = media.Movie("Swordfish",
+                        "A world beneath what we call cyberspace",
+                        "http://www.gstatic.com/tv/thumb/movieposters/27850/p27850_p_v8_aa.jpg",
+                        "https://www.youtube.com/watch?v=mfLizCqjz18")
+
+
+black_hawk = media.Movie ("Black Hawk Down",
+                         "A battle between forces of the United States and Somali militia fighters",
+                         "https://cdn.traileraddict.com/content/columbia-pictures/blackhawkdown-3.jpg",
+                         "https://www.youtube.com/watch?v=AUJ6cxWdZwA")
+
+constantine = media.Movie("Constantine",
+                          "A suicide survivor that had been to hell and back",
+                          "http://www.gstatic.com/tv/thumb/movieposters/35545/p35545_p_v8_ad.jpg",
+                          "https://www.youtube.com/watch?v=DEa508Xmmio")
+ 
+Inception = media.Movie("Inception",
+                        "A thief with the ability to enter people's dreams and steal their secrets",
+                        "https://s-media-cache-ak0.pinimg.com/736x/ff/39/f4/ff39f498bc72f9368a92630b680c195b--minion-movie-minions-despicable-me.jpg",
+                        "https://www.youtube.com/watch?v=8hP9D6kZseM")
+
+
+movies = [forrest_gump, great_gatsby, sword_fish, black_hawk, constantine, Inception]
+#list with all movies created.
+fresh_tomatoes.open_movies_page(movies)
+#open a webpage that showcases the movies.
+
+
